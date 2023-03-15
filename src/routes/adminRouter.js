e83fcee02cde4e5aeb70465031cd94e89947b7d4
Rafaelamelof <rafamelof@gmail.com>
2023-03-15 20:15:24 -0300

    erro css corrigido

--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -12,8 +12,24 @@ router.get('/admin/produtos/cadastro', AdminController.showCadastroProdutos);
 router.get('/admin/produtos/:id/editar', AdminController.showEditarProdutos);
 
 router.post("/admin/login", AdminController.login)
-router.post("/admin/produtos/cadastro", AdminController.storeProduto)
+
+//const upload = multer({dest: 'src/public/images'})
+
+const storage = multer.diskStorage({
+    destination: function(req, file, callback){
+       callback(null, 'src/public/images') 
+    },
+    filename: function(req,file,callback){
+        let fileName = `${Date.now()}_products_${file.originalname}`
+        callback (null, fileName) // perguntar sobre essa estrutura, entender aonde o callback vai ser chamado
+    }
+})
+
+const upload = multer ({storage}) // perguntar
+
+router.post("/admin/produtos/cadastro", upload.single('image'), AdminController.storeProduto)
+
 router.put('/admin/produtos/:id/editar', AdminController. updateProduto)
 router.put('/admin/produtos/:id/deletar', AdminController. deleteProduto)
 
-module.exports = router;
+module.exports = router
